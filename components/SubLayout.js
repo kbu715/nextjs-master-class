dbef4e5ff2766ea012b1ef2368dccda7a458290a
kbu715 <kbu715@gmail.com>
2022-08-02 18:53:34 +0900

    feat: next.js _app.js, Layouts and getLayout

---
+++ components/SubLayout.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function SubLayout({ children }) {
+
+  return (
+    <div>
+      <h1><Link href="/"><a>Home으로</a></Link></h1>
+      {children}
+    </div>
+  )
+
+}
