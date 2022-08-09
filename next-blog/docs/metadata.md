f4d87f8b5171a4e859384acedf161d2f1924dc21
kbu715 <kbu715@gmail.com>
2022-08-10 00:18:47 +0900

    feat: Pre-rendering - data fetching

---
+++ next-blog/docs/metadata.md
@@ -0,0 +1,5 @@
+```markdown
+---
+으로 감싸진 metadata (YAML Front Matter: Metadata 표기 방식 / gray-matter로 파싱)
+---
+```
--- next-blog/pages/index.js
+++ next-blog/pages/index.js
@@ -1,19 +1,47 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/Layout'
+import { getSortedPostsData } from '../lib/posts'
 import utilStyles from '../styles/utils.module.css'
 
-export default function Home() {
+//TODO: 1. API Routes 로 data fetching
+//TODO: 2. getStaticProps에서 API Routes 사용해보기 (getStaticProps에서는 절대 경로 사용하기, fs는 server side 에서만 사용 가능)
+
+export async function getStaticProps() {
+  // getServerSideProps와 dev server에서는 똑같이 동작한다.
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData,
+    },
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Hi there 😛</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
